f3c7ad66da8710a232494d5fb5a76973584b0176
Monali Sonalkar <monalisonalkar@gmail.com>
2025-11-17 18:45:16 +0530

    signup

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 // const HOSTNAME = 'units-api.doqfy.in';
 const HOSTNAME = '192.168.1.3';
-=======
-const HOSTNAME = 'units-api.doqfy.in';
-// const HOSTNAME = '192.168.1.37';
->>>>>>> e5fbc0a5dfdcac57bcfef1867fcf1c1b190e09d0
 export const environment = {
   production: false,
   enableLogging: true,
   HOSTNAME: HOSTNAME,
-<<<<<<< HEAD
   // SERVER_ADDRESS: `https://${HOSTNAME}`,
   SERVER_ADDRESS: `http://${HOSTNAME}:8000`,
-=======
-  SERVER_ADDRESS: `https://${HOSTNAME}`,
-  // SERVER_ADDRESS: `http://${HOSTNAME}:8000`,
->>>>>>> e5fbc0a5dfdcac57bcfef1867fcf1c1b190e09d0
 };
