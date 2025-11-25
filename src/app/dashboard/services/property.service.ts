5de2a985aea1f25c68bbeba14f255468e6b9322a
kunaloptiex <kunal@doqfy.in>
2025-11-25 13:17:08 +0530

    changes

--- src/app/dashboard/services/property.service.ts
+++ src/app/dashboard/services/property.service.ts
@@ -21,8 +21,11 @@ export class PropertyService {
     );
   }
 
-  getDashboardStatistics(): Observable<any> {
-    return this.http.get(`${this.SERVER_ADDRESS}/property/statistics`);
+  getBasicDetails(params: Record<string, any>): Observable<any> {
+    const queryString = this.sharedService.getQueryString(params);
+    return this.http.get(
+      `${this.SERVER_ADDRESS}/create/property/basic${queryString}`
+    );
   }
 
   addBasicDetailsOfProperty(data: Record<string, any>): Observable<any> {
@@ -31,6 +34,13 @@ export class PropertyService {
 
   editBasicDetailsOfProperty(data: Record<string, any>): Observable<any> {
     return this.http.put(`${this.SERVER_ADDRESS}/create/property/basic`, data);
+  }
+
+  getCommercialDetails(params: Record<string, any>): Observable<any> {
+    const queryString = this.sharedService.getQueryString(params);
+    return this.http.get(
+      `${this.SERVER_ADDRESS}/property/commercial/details${queryString}`
+    );
   }
 
   addCommercialDetailsOfProperty(data: Record<string, any>): Observable<any> {
@@ -47,12 +57,26 @@ export class PropertyService {
     );
   }
 
+  getPropertyImages(params: Record<string, any>): Observable<any> {
+    const queryString = this.sharedService.getQueryString(params);
+    return this.http.get(
+      `${this.SERVER_ADDRESS}/property/images/${queryString}`
+    );
+  }
+
   addPropertyImages(data: Record<string, any>) {
     return this.http.post(`${this.SERVER_ADDRESS}/property/images/`, data);
   }
 
   editPropertyImages(data: Record<string, any>) {
     return this.http.put(`${this.SERVER_ADDRESS}/property/images/`, data);
+  }
+
+  getPropertyDocuments(params: Record<string, any>): Observable<any> {
+    const queryString = this.sharedService.getQueryString(params);
+    return this.http.get(
+      `${this.SERVER_ADDRESS}/fetch/property/documents${queryString}`
+    );
   }
 
   addPropertyDocuments(data: Record<string, any>) {
@@ -66,34 +90,6 @@ export class PropertyService {
     return this.http.put(
       `${this.SERVER_ADDRESS}/upload/property/documents`,
       data
-    );
-  }
-
-  getBasicDetails(params: Record<string, any>): Observable<any> {
-    const queryString = this.sharedService.getQueryString(params);
-    return this.http.get(
-      `${this.SERVER_ADDRESS}/create/property/basic${queryString}`
-    );
-  }
-
-  getCommercialDetails(params: Record<string, any>): Observable<any> {
-    const queryString = this.sharedService.getQueryString(params);
-    return this.http.get(
-      `${this.SERVER_ADDRESS}/property/commercial/details${queryString}`
-    );
-  }
-
-  getPropertyImages(params: Record<string, any>): Observable<any> {
-    const queryString = this.sharedService.getQueryString(params);
-    return this.http.get(
-      `${this.SERVER_ADDRESS}/property/images/${queryString}`
-    );
-  }
-
-  getPropertyDocuments(params: Record<string, any>): Observable<any> {
-    const queryString = this.sharedService.getQueryString(params);
-    return this.http.get(
-      `${this.SERVER_ADDRESS}/fetch/property/documents${queryString}`
     );
   }
 }
