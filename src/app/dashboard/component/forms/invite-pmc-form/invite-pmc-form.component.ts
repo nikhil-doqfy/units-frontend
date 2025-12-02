e16ef5205d6089d9f36027d74676487370858a45
Kunal <kunal@doqfy.in>
2025-12-02 23:32:25 +0530

    property assigned and invite pmc

--- src/app/dashboard/component/forms/invite-pmc-form/invite-pmc-form.component.ts
+++ src/app/dashboard/component/forms/invite-pmc-form/invite-pmc-form.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { ModalFormCardComponent } from '../../modal-form-card/modal-form-card.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormService } from '../../../../shared/services/form.service';
 
 @Component({
   selector: 'app-invite-pmc-form',
   standalone: true,
-  imports: [ModalFormCardComponent, TranslateModule],
+  imports: [ModalFormCardComponent, TranslateModule, ReactiveFormsModule],
   templateUrl: './invite-pmc-form.component.html',
   styleUrl: './invite-pmc-form.component.css',
 })
-export class InvitePMCFormComponent {}
+export class InvitePMCFormComponent {
+  private formBuilder = inject(FormBuilder);
+  private formService = inject(FormService);
+
+  isInvalid = this.formService.isInvalid;
+  invitePmcForm = this.formBuilder.group({
+    email: ['', [Validators.required]],
+  });
+}
