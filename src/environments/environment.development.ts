020f3a58f7a046332bb347c6b684231cf6745b14
Nikhil Rane <nikhilrane1992@gmail.com>
2026-03-23 18:25:11 +0530

    Added changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
-const HOSTNAME = 'units-api.doqfy.in';
+const HOSTNAME = 'localhost:8000';
 export const environment = {
   production: false,
   enableLogging: true,
   HOSTNAME: HOSTNAME,
-  SERVER_ADDRESS: `https://${HOSTNAME}`,
+  SERVER_ADDRESS: `http://${HOSTNAME}`,
 };
