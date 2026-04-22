8c1a5c084aaab5677a665c59bc017c6bf253a7b2
Monali Sonalkar <monalisonalkar@gmail.com>
2026-04-22 15:25:10 +0530

    fix: close dropdown on outside click (signature, renew/terminate, receipt, invoice details, share)

--- src/app/component/custom-dropdown/custom-dropdown.component.ts
+++ src/app/component/custom-dropdown/custom-dropdown.component.ts
@@ -1,16 +1,31 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 import { ArrowDownIconComponent } from '../../shared/component/icons/arrow-down-icon/arrow-down-icon.component';
 import { ReceiptIconComponent } from '../../icons/receipt-icon/receipt-icon.component';
 import { CommonModule } from '@angular/common';
+import { ArrowUpIconComponent } from '../../shared/component/icons/arrow-up-icon/arrow-up-icon.component';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-custom-dropdown',
   standalone: true,
-  imports: [ArrowDownIconComponent, CommonModule],
+  imports: [
+    ArrowDownIconComponent,
+    CommonModule,
+    ArrowUpIconComponent,
+    NgbDropdownModule,
+  ],
   templateUrl: './custom-dropdown.component.html',
   styleUrl: './custom-dropdown.component.css',
 })
 export class CustomDropdownComponent {
+  isOpen = false;
   @Input() title: string = '';
   @Input() hasIcon: boolean = true;
 
@@ -20,6 +35,7 @@ export class CustomDropdownComponent {
   selectedReceiptType = '';
 
   @Output() detailViewChanges = new EventEmitter<boolean>();
+  constructor(private eRef: ElementRef) {}
 
   selectReceiptType(type: string) {
     this.selectedReceiptType = type;
@@ -28,12 +44,35 @@ export class CustomDropdownComponent {
   }
 
   toggleDropdown() {
-    if (this.showReceiptDropdown || this.showMonthDropdown) {
+    this.isOpen = !this.isOpen;
+    if (this.isOpen) {
+      this.showReceiptDropdown = true;
+      this.showMonthDropdown = false;
+    } else {
       this.showReceiptDropdown = false;
       this.showMonthDropdown = false;
-      return;
     }
 
     this.showReceiptDropdown = true;
+  }
+  onDropdownOpenChange(open: boolean) {
+    this.isOpen = open;
+    if (!open) {
+      this.showReceiptDropdown = false;
+      this.showMonthDropdown = false;
+    }
+  }
+
+  closeDropdown() {
+    this.isOpen = false;
+    this.showReceiptDropdown = false;
+    this.showMonthDropdown = false;
+  }
+  // 👉 OUTSIDE CLICK DETECTION (MAIN LOGIC)
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.closeDropdown();
+    }
   }
 }
