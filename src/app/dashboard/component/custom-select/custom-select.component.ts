e0b19e1795d76abbe96abee4c683579bc5bad8f9
Monali Sonalkar <monalisonalkar@gmail.com>
2026-05-27 18:13:30 +0530

    changes

--- src/app/dashboard/component/custom-select/custom-select.component.ts
+++ src/app/dashboard/component/custom-select/custom-select.component.ts
@@ -105,6 +105,51 @@ export class CustomSelectComponent implements OnInit, ControlValueAccessor {
 
  
 
+  // toggleDropdown() {
+  //   if (this.isDisabled) return;
+
+  //   this.isDropdownOpen = !this.isDropdownOpen;
+  //   if (this.isDropdownOpen) {
+  //     this.dropdownService.notifyOpen(this);
+  //     const rect = this.el.nativeElement.getBoundingClientRect();
+  //     const dropdownHeight = 224;
+  //     const spaceBelow = window.innerHeight - rect.bottom;
+  //     const openUpward =
+  //       spaceBelow < dropdownHeight && rect.top > dropdownHeight;
+
+  // this.dropdownStyle = openUpward
+  //   ? {
+  //       position: 'fixed',
+  //       bottom:   `${window.innerHeight - rect.top + 4}px`,
+  //       left:     `${rect.left}px`,
+  //       width:    `${rect.width}px`,
+  //       'z-index': '9999',
+  //     }
+  //   : {
+  //       position: 'fixed',
+  //       top:      `${rect.bottom + 4}px`,
+  //       left:     `${rect.left}px`,
+  //       width:    `${rect.width}px`,
+  //       'z-index': '9999',
+  //     };
+  //     this.dropdownStyle = openUpward
+  //       ? {
+  //           position: 'absolute',
+  //           bottom: 'calc(100% + 4px)',
+  //           right: '0',
+  //           width: '100%',
+  //           'z-index': '9999',
+  //         }
+  //       : {
+  //           position: 'absolute',
+  //           top: 'calc(100% + 4px)',
+  //           right: '0',
+  //           width: '100%',
+  //           'z-index': '9999',
+  //         };
+  //   }
+  //   this.onTouched();
+  // }
   toggleDropdown() {
     if (this.isDisabled) return;
 
