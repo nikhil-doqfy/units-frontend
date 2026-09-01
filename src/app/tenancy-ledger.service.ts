d8abed0948209e8844bc3f54c6e5945790be06a1
Monali <monali@doqfy.in>
2026-09-01 12:49:15 +0530

    added services

---
+++ src/app/tenancy-ledger.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SharedService } from './shared.service';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TenancyLedgerService {
+  private http = inject(HttpClient);
+  private sharedService = inject(SharedService);
+  private SERVER_ADDRESS = environment.SERVER_ADDRESS;
+  constructor() {}
+  getTenancyLedger(params?: any): Observable<any> {
+    return this.http.get(`${this.SERVER_ADDRESS}/tenancy-ledger`, {
+      params,
+    });
+  }
+  exportTenacyLedger(params: Record<string, any> = {}): Observable<Blob> {
+    const queryString = this.sharedService.getQueryString({
+      ...params,
+      export: 'true',
+    });
+    return this.http.get(
+      `${this.SERVER_ADDRESS}/tenancy-ledger${queryString}`,
+      {
+        responseType: 'blob',
+      },
+    );
+  }
+}
